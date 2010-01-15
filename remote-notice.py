d26dbfdc53772d9963b5d693bf14ae65b3ee5978
Tim Horton <tmhorton@gmail.com>
2010-01-14 17:25:32 -0800

    Added escape to messages

--- remote-notice.py
+++ remote-notice.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 from optparse import OptionParser
+
+import pynotify
+import re
 import simplejson as json
 import socket
-import pynotify
 
 def run(port):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind(('',port))
         s.listen(10)
         while 1:
@@ -30,18 +33,23 @@ def handleResp(data):
         d = json.loads(data)
         msg(d['title'], d['msg'])
     except Exception as e:
+        print data
         print e
+
+def escape(s):
+    return re.sub('''(['"])''', '', s)
 
 def msg(title, msg, appname='remote-notice'):
     if pynotify.init(appname):
-        n = pynotify.Notification(title, msg)
+        n = pynotify.Notification(escape(title), escape(msg), "dialog-warning")
         n.show()
     else:
         print "Unable to send notification"
 
 if __name__ == '__main__':
     parser = OptionParser()
-    parser.add_option('-p', '--port', dest='port', help='server port', type='int', default=33333)
+    parser.add_option('-p', '--port', dest='port', help='server port', \
+                                      type='int', default=33333)
     (opts, args) = parser.parse_args()
     run(opts.port)
 
